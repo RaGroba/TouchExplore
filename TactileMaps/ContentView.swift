f568480c50519b65c8252797580656db189c6f7f
Dario Merz <dme@hinderlingvolkart.com>
2020-04-25 15:25:04 +0200

    Initial Commit

---
+++ TactileMaps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TactileMaps
+//
+//  Created by Dario Merz on 25.04.20.
+//  Copyright © 2020 Dario Merz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
